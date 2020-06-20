7e18c9ef391446b768ba1f86702aed188bf70db0
Axel Chauvin <axelchauvin@gmail.com>
2020-06-20 20:23:49 +0200

    Added simple HMLT page structure, basic CSS and menu animations

---
+++ js/page_animations.js
@@ -0,0 +1,40 @@
+const instruPanel = document.querySelector(".instruction-panel");
+const instruToggle = document.getElementById("instruction-toggle");
+
+function toggleInstructionPanel() {
+  if (instruPanel.classList.contains("instru-pan-hover")) {
+    instruPanel.classList.toggle("instru-pan-hover");
+  } else {
+    instruPanel.classList.toggle("instru-pan-is-hidden");
+  }
+}
+
+function moveInInstructionPanel() {
+  if (instruPanel.classList.contains("instru-pan-is-hidden")) {
+    instruPanel.classList.toggle("instru-pan-hover");
+    instruPanel.classList.toggle("instru-pan-is-hidden");
+  }
+}
+
+function moveOutInstructionPanel() {
+    if (instruPanel.classList.contains("instru-pan-hover")) {
+        instruPanel.classList.toggle("instru-pan-hover");
+        instruPanel.classList.toggle("instru-pan-is-hidden");
+      }
+}
+
+function toggleGameInfoPanel() {
+  document
+    .querySelector(".game-info-panel")
+    .classList.toggle("game-info-pan-is-hidden");
+}
+
+function startGame() {
+  toggleGameInfoPanel();
+  this.parentNode.style.display = "none";
+}
+
+instruToggle.onclick = toggleInstructionPanel;
+instruToggle.onmouseover = moveInInstructionPanel;
+instruToggle.onmouseleave = moveOutInstructionPanel;
+document.getElementById("btn-start").onclick = startGame;
